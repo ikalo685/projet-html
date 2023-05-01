be0f5758a1900adf679b6748300ddc0e9492c40f
Prince <alvinprinceikalo01021998@gmail.com>
2023-05-01 22:02:29 +0300

    fist_projet

---
+++ New folder/first_projet.js
@@ -0,0 +1,10 @@
+
+const btn1 = document.getElementById(btn1);
+const btn2 = document.getElementById(btn2);
+const btn3 = document.getElementById(btn3);
+const btn4 = document.getElementById(btn4);
+
+btn1.onclick = () =>document.body.styleSheets.backgroundcolor ="red";
+btn2.onclick = () =>document.body.styleSheets.backgroundcolor ="green";
+btn3.onclick = () =>document.body.styleSheets.backgroundcolor ="orange";
+btn4.onclick = () =>document.body.styleSheets.backgroundcolor ="yellow";
